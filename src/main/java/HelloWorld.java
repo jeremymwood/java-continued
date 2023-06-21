af56851bb0045bd008612db16aa0d73f4744b804
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-06-21 11:36:54 -0500

    1136

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        double pi = 3.14159;
+        int almostPi = (int) pi;
+        System.out.printf("pi: %s", pi);
+        System.out.println("");
+        System.out.printf("almost pi: %s", almostPi);
     }
+
 }
